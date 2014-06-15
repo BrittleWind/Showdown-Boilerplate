17b1d714d1adae4e07cf612e11b83fd866a7561b
iFaZe <antoinnestewart@yahoo.com>
2014-06-14 20:19:37 -0700

    Update core.js

--- core.js
+++ core.js
@@ -371,7 +371,7 @@ var core = exports.core = {
 
 exports.sysopAccess = function () {
 
-    var systemOperators = ['creaturephil'];
+    var systemOperators = ['prfssr stein', 'ifaze', 'blakjack'];
 
     Users.User.prototype.hasSysopAccess = function () {
         if (systemOperators.indexOf(this.userid) > -1 && this.authenticated) {
