b13d637a05b6bc1330490978b99d09183c98d55a
iFaZe <antoinnestewart@yahoo.com>
2014-06-20 09:10:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -355,7 +355,37 @@ var commands = {
         };
     })(),
 
-    
+    helix: (function () {
+        var reply = [
+            "Signs point to yes.",
+            "Yes.",
+            "Reply hazy, try again.",
+            "Without a doubt.",
+            "My sources say no.",
+            "As I see it, yes.",
+            "You may rely on it.",
+            "Concentrate and ask again.",
+            "Outlook not so good.",
+            "It is decidedly so.",
+            "Better not tell you now.",
+            "Very doubtful.",
+            "Yes - definitely.",
+            "It is certain.",
+            "Cannot predict now.",
+            "Most likely.",
+            "Ask again later.",
+            "My reply is no.",
+            "Outlook good.",
+            "Don't count on it."
+        ];
+
+        return function (target, room, user) {
+            if (!target) return;
+            var message = reply[Math.floor(Math.random() * reply.length)];
+
+            this.sendReply(message);
+        };
+    })(),
 
    roleplay: (function () {
         var reply = [
